5efd12a88da10c5d6cca4fb86593a9d4f48937f4
simone.capone <simo.capone.1997.7@outlook.it>
2025-02-06 08:16:21 +0100

    1 - Config

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,7 @@
+import io.quarkus.runtime.Quarkus;
+
+public class MainApp {
+    public static void main(String... args) {
+            Quarkus.run(args); // Avvia l'applicazione Quarkus
+        }
+}
